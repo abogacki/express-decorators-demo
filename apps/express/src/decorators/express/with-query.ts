3611990f959933c7596dd02bc37e481c3bfd4f8e
Andrzej Bogacki <anbogacki@gmail.com>
2024-11-24 20:24:50 +0100

    Remove console logs

--- apps/express/src/decorators/express/with-query.ts
+++ apps/express/src/decorators/express/with-query.ts
@@ -8,15 +8,11 @@ type QueryValue = string | string[];
 export const withQuery: DecoratorFactory<Args, RequestDescriptor> =
   (query: string, validate: (val: QueryValue) => boolean) =>
   (_target, _propertyKey, descriptor) => {
-    console.log({ _target, _propertyKey, descriptor });
-
     const originalMethod = descriptor.value;
 
     descriptor.value = function (...args) {
       const [req] = args;
       const queryValue = req.query[query] as QueryValue;
-
-      console.log({ queryValue, req, query });
 
       if (!validate(queryValue)) {
         throw new Error('Query validation error: missing value for: ' + query);
